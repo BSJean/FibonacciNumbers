3b35aa21ba6d362f3bd90e86a13c4f02a0cf0ce2
Admin <Admin@BS>
2019-03-01 21:23:26 +0300

    Wrote code

---
+++ FibonacciNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int n in FibonacciNumbers(20))
+            {
+                Console.WriteLine(n);
+            }
+        }
+
+        public static List<int> FibonacciNumbers(int count)
+        {
+            List<int> numbers = new List<int> { 1, 1 };
+            if (count <= 0)
+                throw new ArgumentException("Count Cannot Be Less Or Equal Zero.");
+            if (count == 1)
+                return new List<int> { 1 };
+                checked
+            {
+                try
+                {
+                    while (numbers.Count < count)
+                    {
+                        numbers.Add(numbers[numbers.Count - 1] + numbers[numbers.Count - 2]);
+                    }
+                }
+                catch (OverflowException)
+                {
+                    throw new OverflowException($"Number Is More Than {int.MaxValue}.");
+                }
+            }     
+            return numbers;
+        }
+    }
+}
---
+++ FibonacciNumbersTests/UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciNumbersTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CountLessOneThrowArgumentException()
+        {
+            //Arrange
+            int count = 0;
+            List<int> expected;
+
+            //Act, Assert
+            expected=FibonacciNumbers.Program.FibonacciNumbers(count);
+        }
+
+        [TestMethod]
+        public void CountEqualOneResultIsOne()
+        {
+            //Arrange
+            int count = 1;
+            List<int> expected=new List<int> { 1 };
+
+            //Act
+            List<int> actual = FibonacciNumbers.Program.FibonacciNumbers(count);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CountEqualTwoResultIsOne()
+        {
+            //Arrange
+            int count = 2;
+            List<int> expected;
+
+            //Act
+            expected = FibonacciNumbers.Program.FibonacciNumbers(count);
+            List<int> actual = new List<int> { 1, 1 };
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+                
+        [TestMethod]        
+        public void CountMoreThanThreeAndResultLessOrEqualIntMaxValue()
+        {
+            //Arrange
+            int count = 20;
+            List<int> expected;
+
+            //Act
+            expected = FibonacciNumbers.Program.FibonacciNumbers(count);
+            List<int> actual = new List<int> { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765 };
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OverflowException))]
+        public void ResultMoreThanIntMaxValueThrowOverflowException()
+        {
+            //Arrange
+            int count = 50;
+            List<int> expected;
+
+            //Act, Assert
+            expected = FibonacciNumbers.Program.FibonacciNumbers(count);
+        }
+    }
+}
